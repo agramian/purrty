3c71cfd13c8fbd35553ce17ef724a37bf60d141e
Abtin Gramian <abtin.gramian@gmail.com>
2017-06-20 17:24:06 -0700

    added monolith library module and upload handling

---
+++ library/src/main/java/com/abtingramian/purtee/Purtee.java
@@ -0,0 +1,11 @@
+package com.abtingramian.purtee;
+
+/**
+ * A placeholder class so there is some material to make an AAR. In theory we should just be
+ * able to publish the monolith as a pom with only dependencies and no code but Android gradle
+ * does not respect that properly.
+ *
+ * referenced from: https://github.com/firebase/FirebaseUI-Android/blob/master/library/src/main/java/com/firebase/ui/FirebaseUI.java
+ */
+public class Purtee {
+}
