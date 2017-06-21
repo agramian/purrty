359305809900f4c78132bbea6806dee6a9a6fda7
Abtin Gramian <abtin.gramian@gmail.com>
2017-06-20 18:40:50 -0700

    additional util classes

---
+++ util/src/main/java/com/abtingramian/purtee/util/DimensionUtil.java
@@ -0,0 +1,16 @@
+package com.abtingramian.purtee.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+public class DimensionUtil {
+
+    public static int dpToPx(Context context, float dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    }
+
+    public static int spToPx(Context context, float sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources().getDisplayMetrics());
+    }
+
+}
---
+++ util/src/main/java/com/abtingramian/purtee/util/DisplayUtil.java
@@ -0,0 +1,29 @@
+package com.abtingramian.purtee.util;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class DisplayUtil {
+
+    public static int getDisplayDensityDp(@NonNull Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return (int) (metrics.density * 160f);
+    }
+
+    public static Display getDisplay(@NonNull Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        return wm.getDefaultDisplay();
+    }
+
+    public static int getLargestDimensionSize(@NonNull Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        return Math.max(point.x, point.y);
+    }
+
+}
